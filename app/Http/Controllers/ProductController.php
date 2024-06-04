fe35c84d41ca7091e1bbf351b868ed4203a16f9a
hiamnaive <leehung240399@gmail.com>
2024-06-04 22:21:07 +0700

    hung

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Category;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    private $cate;
+
+    function __construct(Category $cate)
+    {
+        $this->cate = $cate;
+    }
+    
+    function index(){
+        $cate = $this->cate->category();
+        return response()->json
+        ([
+            'cate' => $cate
+        ], 200);
+    }
+}
--- app/Models/Category.php
+++ app/Models/Category.php
@@ -4,10 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class Category extends Model
 {
     use HasFactory;
     protected $table = 'category';
     protected $primaryKey = 'id_category';
+    
+    public function category()
+    {
+        $cate = DB::table('category')->get();
+        return $cate;
+    }
 }
