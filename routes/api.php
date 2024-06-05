f072e644910aad0b48033010fefe563c90a1bef6
hiamnaive <leehung240399@gmail.com>
2024-06-05 17:23:04 +0700

    a

--- routes/api.php
+++ routes/api.php
@@ -8,3 +8,8 @@ use Illuminate\Support\Facades\Route;
 Route::prefix('/cate') -> group(function () {
     Route::get('/', [ProductController::class, 'cate']);
 });
+Route::prefix('/product') -> group(function () {
+    Route::get('/', [ProductController::class, 'product']);
+    Route::get('{id_product}', [ProductController::class, ['productDetail']]);
+    Route::get('{id_category}', [ProductController::class, ['productCate']]);
+});
