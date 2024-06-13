7e2fed473ad4201acb32233560f8e07e971cdeb9
AKAepzai <thang0866808370@gmail.com>
2024-06-13 23:47:59 +0700

    imageThang

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\AdminController;
-Route::prefix('/admin')->group(function(){
+
+Route::prefix('/admin')->group(function () {
     Route::get('/', [AdminController::class, 'index']);
 });
