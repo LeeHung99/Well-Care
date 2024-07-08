1e4a07da8c2b75ff396f6c6d8c27ef923e85439a
Huy | apikey:2faa729edff722f19296d472459b39ffbed8ddff2221c6fcdc82975f85a3263b <huy.lg@fsofts.com>
2024-07-08 22:19:35 +0700

    huy comment

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -10,21 +10,21 @@ class CommentController extends Controller
     public function comment(Request $request)
     {
 
-        return response()->json(['success' => $request]);
+        // return response()->json(['success' => $request]);
         // $validatedData = $request->validate([
         //     'id_user' => 'required|integer|exists:users,id',
         //     'id_product' => 'required|integer|exists:products,id',
         //     'content' => 'required|string|max:255',
         // ]);
 
-        // $cleanContent = strip_tags($validatedData['content']);
+        $cleanContent = strip_tags($request->content);
 
-        // Comments::create([
-        //     'id_user' => $validatedData['id_user'],
-        //     'id_product' => $validatedData['id_product'],
-        //     'content' => $cleanContent,
-        // ]);
+        Comments::create([
+            'id_user' => $request->id_user,
+            'id_product' => $request->id_product,
+            'content' => $cleanContent,
+        ]);
 
-        // return response()->json(['success' => 'Thêm bình luận thành công']);
+        return response()->json(['success' => 'Thêm bình luận thành công']);
     }
 }
